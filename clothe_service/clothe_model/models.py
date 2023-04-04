d8035cb5b6c267cf501432835fa2dbebfa4b45b9
hoang <hoangnv2782001@gmail.com>
2023-04-04 07:37:05 +0700

    clothes_service 2 api get post

---
+++ clothe_service/clothe_model/models.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+from django.db import models
+
+# Create your models here.
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+
+# This is our model for user registration.
+class Clothe(models.Model):
+    # The following are the fields of our table.
+    clothe_id = models.CharField(max_length=10)
+    category = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
+    availability = models.CharField(max_length=15)
+    price = models.IntegerField()
+    quantity = models.IntegerField(null=True)
+    size = models.IntegerField()
+    color = models.CharField()
+    description = models.TextField()
+    brand = models.CharField()
+    #
+    # # It will help to print the values.
+    # def __str__(self):
+    #     return '%s %s %s %s %s' % (self.product_id, self.product_category,
+    #                                self.product_name, self.availability, self.price)
---
+++ clothe_service/clothe_model/views.py
@@ -0,0 +1,82 @@
+from __future__ import unicode_literals
+
+from datetime import date
+
+from django.shortcuts import render
+
+# Create your views here.
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+
+from django.shortcuts import render
+import json
+from django.views.decorators.csrf import csrf_exempt
+from clothe_model.models import Clothe
+
+
+def insert_clothe(shoe_id,title,category,brand,availability,price,description,quantity,size,color):
+    book_data = Clothe(clothe_id=shoe_id,name=title,category=category,brand=brand,quantity=quantity,availability=availability,description=description,price=price,size=size,color=color)
+    book_data.save()
+    return 1
+
+
+@csrf_exempt
+def get_clothes(request):
+    data = []
+    resp = {}
+    insert_clothe('11112', 'ao', 'sơ mi', 'vn',  'avialable', 100, 'giay hay',20,34,'red')
+    # This will fetch the data from the database.
+    prodata = Clothe.objects.all()
+    for tbl_value in prodata.values():
+        data.append(tbl_value)
+    # If data is available then it returns the data.
+    if data:
+        resp['status'] = 'Success'
+        resp['status_code'] = '200'
+        resp['data'] = data
+    else:
+        resp['status'] = 'Failed'
+        resp['status_code'] = '400'
+        resp['message'] = 'Data is not available.'
+    return HttpResponse(json.dumps(resp,cls=DateEncoder), content_type='application/json')
+
+@csrf_exempt
+def add_clothe(request):
+    shoe_id = request.POST.get('shoe_id')
+    name = request.POST.get('name')
+    category = request.POST.get('category')
+    brand = request.POST.get('brand')
+    availability = 'available'
+    price = request.POST.get('price')
+    description = request.POST.get('description')
+    quantity = request.POST.get('quantity')
+    size = request.POST.get('size')
+    color = request.POST.get('color')
+    resp = {}
+    if shoe_id and name and price and quantity and category and description and size and brand and color:
+        ### It will call the store data function.
+        respdata = insert_clothe(shoe_id=shoe_id,title=name,price=price,quantity=quantity,availability=availability,description=description,category=category,size=size,brand=brand,color=color)
+        ### If it returns value then will show success.
+        if respdata:
+            resp['status'] = 'Success'
+            resp['status_code'] = '200'
+            resp['message'] = 'completed.'
+        ### If it is returning null value then it will show failed.
+        else:
+            resp['status'] = 'Failed'
+            resp['status_code'] = '400'
+            resp['message'] = 'Please try again.'
+        ### If any mandatory field is missing then it will be through a failed message.
+    else:
+        resp['status'] = 'Failed'
+        resp['status_code'] = '400'
+        resp['message'] = 'All fields are mandatory.'
+    return HttpResponse(json.dumps(resp), content_type='application/json')
+
+
+class DateEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, date):
+            return obj.strftime('%d-%m-%Y')
+        return super().default(obj)
+
